eb542f9aca3ed545a7012908abfd6af10367fcdf
Adam Ali <adamka2000@outlook.com>
2023-02-10 14:40:19 +0000

    Initial Commit

---
+++ JobHuntr/JobHuntrApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobHuntrApp.swift
+//  JobHuntr
+//
+//  Created by Adam Ali on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct JobHuntrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
