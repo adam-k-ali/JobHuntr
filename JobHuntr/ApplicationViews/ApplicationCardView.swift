ea3d9a201bca8cef5bda89b5918a381b539ff079
Adam Ali <adamka2000@outlook.com>
2023-03-03 12:50:14 +0000

    New layout for application card

--- JobHuntr/ApplicationViews/ApplicationCardView.swift
+++ JobHuntr/ApplicationViews/ApplicationCardView.swift
@@ -19,12 +19,24 @@ struct ApplicationCardView: View {
         ZStack {
             HStack {
                 VStack(alignment: .leading, spacing: 8) {
-                    if let company = company {
-                        Text(company.name)
-                            .font(.headline)
-                    } else {
-                        Text("Unknown Company")
-                            .font(.headline)
+                    HStack {
+                        if let company = company {
+                            Text(company.name)
+                                .font(.headline)
+                        } else {
+                            Text("Unknown Company")
+                                .font(.headline)
+                        }
+                        Spacer()
+                        if let dateApplied = application.dateApplied {
+                            let dateStr = formatDateString(from: dateApplied.foundationDate)
+                            
+                            HStack {
+                                Text(dateStr)
+                                    .font(.headline)
+                                    .foregroundColor(.gray)
+                            }
+                        }
                     }
                     if let jobTitle = application.jobTitle {
                         Text(jobTitle)
@@ -34,19 +46,12 @@ struct ApplicationCardView: View {
                     HStack {
                         ZStack {
                             Capsule()
-                                .fill(Color(uiColor: .systemGray))
+                                .fill(application.currentStage!.color)
+//                                .fill(Color(uiColor: .systemGray))
                                 .frame(width: 128, height: 32)
                             Text(application.currentStage!.name)
                         }
-                        if let dateApplied = application.dateApplied {
-                            let dateStr = formatDateString(from: dateApplied.foundationDate)
-                            
-                            HStack {
-                                Image(systemName: "calendar")
-                                Text("Applied \(dateStr)")
-                                    .font(.subheadline)
-                            }
-                        }
+                        
                     }
                 }
             }
@@ -64,32 +69,11 @@ struct ApplicationCardView: View {
     
     func formatDateString(from date: Date) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .short
+        dateFormatter.dateStyle = .long
         dateFormatter.timeStyle = .none
         
         return dateFormatter.string(from: date)
     }
-}
-
-extension UIColor {
-    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
-            var red: CGFloat = 0
-            var green: CGFloat = 0
-            var blue: CGFloat = 0
-            var alpha: CGFloat = 0
-            getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-
-            return (red, green, blue, alpha)
-        }
-}
-
-extension Color {
-    init(uiColor: UIColor) {
-            self.init(red: Double(uiColor.rgba.red),
-                      green: Double(uiColor.rgba.green),
-                      blue: Double(uiColor.rgba.blue),
-                      opacity: Double(uiColor.rgba.alpha))
-        }
 }
 
 struct ApplicationCardView_Previews: PreviewProvider {
--- JobHuntr/ModelExtensions.swift
+++ JobHuntr/ModelExtensions.swift
@@ -12,17 +12,17 @@ extension ApplicationStage {
     var color: Color {
         switch self {
             case .applied:
-                return .yellow
+            return Color(hex: "#ef9b20")
             case .preInterview:
-                return .blue
+            return Color(hex: "#f46a9b")
             case .interviewing:
-                return .blue
+            return Color(hex: "#87bc45")
             case .offer:
-                return .yellow
+            return Color(hex: "#f46a9b")
             case .rejection:
-                return .red
+            return Color(hex: "#ea5545")
             case .accepted:
-            return .green
+            return Color(hex: "#bdcf32")
         }
     }
     var name: String {
@@ -41,4 +41,50 @@ extension ApplicationStage {
                 return "Offer Accepted"
         }
     }
+}
+
+extension UIColor {
+    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+            var red: CGFloat = 0
+            var green: CGFloat = 0
+            var blue: CGFloat = 0
+            var alpha: CGFloat = 0
+            getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+            return (red, green, blue, alpha)
+        }
+}
+
+extension Color {
+    init(uiColor: UIColor) {
+            self.init(red: Double(uiColor.rgba.red),
+                      green: Double(uiColor.rgba.green),
+                      blue: Double(uiColor.rgba.blue),
+                      opacity: Double(uiColor.rgba.alpha))
+        }
+    
+    init(hex: String) {
+            let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+            var int: UInt64 = 0
+            Scanner(string: hex).scanHexInt64(&int)
+            let a, r, g, b: UInt64
+            switch hex.count {
+            case 3: // RGB (12-bit)
+                (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+            case 6: // RGB (24-bit)
+                (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+            case 8: // ARGB (32-bit)
+                (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+            default:
+                (a, r, g, b) = (1, 1, 1, 0)
+            }
+
+            self.init(
+                .sRGB,
+                red: Double(r) / 255,
+                green: Double(g) / 255,
+                blue:  Double(b) / 255,
+                opacity: Double(a) / 255
+            )
+        }
 }
