8cd32968a7fe15ffa2085f37e2d6a3101cb9d54b
Adam Ali <adamka2000@outlook.com>
2023-03-04 12:33:20 +0000

    Put SettingsView in a List, with section headers

---
+++ JobHuntr/ApplicationViews/ApplicationsList.swift
@@ -0,0 +1,20 @@
+//
+//  ApplicationsList.swift
+//  JobHuntr
+//
+//  Created by Adam Ali on 28/02/2023.
+//
+
+import SwiftUI
+
+struct ApplicationsList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ApplicationsList_Previews: PreviewProvider {
+    static var previews: some View {
+        ApplicationsList()
+    }
+}
