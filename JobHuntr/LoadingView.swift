faf6432f7b153eed1a588bfbc6ceac08143d266b
Adam Ali <adamka2000@outlook.com>
2023-03-19 02:46:04 +0000

    New Loading View

--- JobHuntr/LoadingView.swift
+++ JobHuntr/LoadingView.swift
@@ -9,21 +9,29 @@ import SwiftUI
 
 struct LoadingView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            Image(uiImage: UIImage(named: "AppIcon") ?? UIImage())
-                .resizable()
-                .clipShape(RoundedRectangle(cornerRadius: 8.0))
-                .frame(width: 128, height: 128)
-            ProgressView()
-                .padding(20)
-            Spacer()
+        ZStack {
+            Color("AppBackground")
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image(uiImage: UIImage(named: "AppIcon") ?? UIImage())
+                    .resizable()
+                    .clipShape(RoundedRectangle(cornerRadius: 8.0))
+                    .frame(width: 128, height: 128)
+                Spacer()
+                ProgressCircle()
+                    .frame(width: 48.0, height: 48.0)
+                    .padding(20)
+//                ProgressView()
+//                    .padding(20)
+                Spacer()
+            }
         }
     }
 }
 
-//struct LoadingView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        LoadingView()
-//    }
-//}
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
